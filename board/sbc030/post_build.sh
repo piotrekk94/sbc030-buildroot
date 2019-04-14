9187947c023e306e00f62581d3e03c4c2fc61fc2
Piotr Binkowski <piotr.binkowski1@gmail.com>
2019-04-14 12:05:57 +0200

    sbc030: add building u-boot and kernel

--- board/sbc030/post_build.sh
+++ board/sbc030/post_build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo 'SBC030 post build script'
+
+out='linux'
+
+cd $1/boot
+
+m68k-linux-objcopy -O binary vmlinux $out.bin
+lzop -f -9 $out.bin
+mkimage -A m68k -O linux -C lzo -a 0x1000 -d $out.bin.lzo $out.u
+
+rm -f $out.bin $out.bin.lzo vmlinux
+
